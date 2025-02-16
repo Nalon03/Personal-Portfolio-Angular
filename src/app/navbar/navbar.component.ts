4d7b4ea23a975ff0635a48f50135c1c2ee82c3c7
nalon03-github <nalongrace03@gmail.com>
2025-02-16 20:09:44 +0000

    add smaller screen responsiveness

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-  constructor(private route: Router) {}
+export class NavbarComponent implements OnInit {
+  isMenuOpen = false;
+
+  constructor(
+    private route: Router,
+    private breakpointObserver: BreakpointObserver
+  ) {}
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe(['(min-width: 601px)'])
+      .subscribe((result) => {
+        if (result.matches) {
+          this.isMenuOpen = false;
+        }
+      });
+  }
 
   navigateHome() {
     this.route.navigate(['home']);
@@ -29,5 +46,9 @@ export class NavbarComponent {
   }
   navigatePortfolio() {
     this.route.navigate(['portfolio']);
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 }
